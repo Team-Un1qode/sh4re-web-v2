5aee1572a0ee62e794c272ce3c09ca26e727a89e
chaeyn <chaulfe@gmail.com>
2025-05-27 18:11:30 +0900

    feat: react hook form 테스트

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,29 @@
 import * as S from "./index.style.ts";
+import { useForm } from "react-hook-form";
+
+type LoginFormInputs = {
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
+
+  const onSubmit = (data: LoginFormInputs) => {
+    console.log(data);
+  };
+
   return (
     <S.Container>
       <S.BackgroundWrapper>
         <S.Background />
       </S.BackgroundWrapper>
       <S.LoginContainer>
-        <S.LoginBox>
+        <S.LoginBox onSubmit={handleSubmit(onSubmit)}>
           <S.Header>
             <S.LoginTitle>로그인</S.LoginTitle>
             <S.LoginSubTitle>서비스를 시작하려면 로그인하세요.</S.LoginSubTitle>
@@ -15,15 +31,35 @@ const Login = () => {
           <S.InputContainer>
             <S.InputBox>
               <S.InputIcon src='/login-user-icon.svg'></S.InputIcon>
-              <S.Input placeholder='아이디를 입력하세요.'></S.Input>
+              <S.Input
+                placeholder='아이디를 입력하세요.'
+                {...register("username", { required: "아이디를 입력하세요." })}
+              ></S.Input>
             </S.InputBox>
+            {errors.username && (
+              <span
+                style={{ color: "red", fontSize: "0.85rem", marginLeft: "5px" }}
+              >
+                {errors.username.message}
+              </span>
+            )}
             <S.InputBox>
               <S.InputIcon src='/login-password-icon.svg'></S.InputIcon>
               <S.Input
                 placeholder='비밀번호를 입력하세요.'
                 type='password'
+                {...register("password", {
+                  required: "비밀번호를 입력하세요.",
+                })}
               ></S.Input>
             </S.InputBox>
+            {errors.username && (
+              <span
+                style={{ color: "red", fontSize: "0.85rem", marginLeft: "5px" }}
+              >
+                {errors.password?.message}
+              </span>
+            )}
           </S.InputContainer>
           <S.LoginButton>로그인</S.LoginButton>
           <S.ForgetPasswordContainer>
