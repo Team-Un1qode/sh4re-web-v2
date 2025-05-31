6d668ad95789c4fc505288414f4fe4efee0b7ee5
chaeyn <chaulfe@gmail.com>
2025-05-31 13:55:18 +0900

    feat: add register page route

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login/index";
+import Register from "../pages/register/index";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
