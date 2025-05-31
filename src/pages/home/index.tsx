8a35290f789c19f830b2901c1bd7c6c043784968
bbqcream <imground11@gmail.com>
2025-05-31 14:38:53 +0900

    feat: 사이드바 뷰 만들기

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../components/common/sidebar";
+import * as S from "./style";
+
+const Home = () => {
+    return (
+        <S.Container>
+            <Sidebar />
+        </S.Container>
+    );
+};
+
+export default Home;
---
+++ src/pages/home/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { COLOR } from "../../style/color/color";
+
+export const Container = styled.div`
+
+    background-color: ${COLOR.background};
+`;
