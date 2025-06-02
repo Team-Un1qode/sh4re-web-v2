b2a4f1a7caf4fdfebdcc6161d523ebe04497634d
chaeyn <chaulfe@gmail.com>
2025-06-02 22:55:49 +0900

    feat: 더미 데이터 추가

--- src/components/common/home/assignment/list/style.ts
+++ src/components/common/home/assignment/list/style.ts
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: white;
-    align-items: center;
-    box-shadow: 0px 3px 24px 0px rgba(0, 0, 0, 0.02);
-    display: flex;
-    flex-direction: column;
-    border-radius: 0.25rem;
-    &:first-child {
-        grid-row: 1 / span 2;
-    }
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  align-items: center;
+  box-shadow: 0px 3px 24px 0px rgba(0, 0, 0, 0.02);
+  display: flex;
+  flex-direction: column;
+  border-radius: 0.25rem;
+  &:first-child {
+    grid-row: 1 / span 2;
+  }
 `;
 
 export const AssignmentList = styled.div`
-    width:100%;
-    display:flex;
-    align-items: center;
-    flex-direction:column;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  overflow: auto;
+`;
--- src/components/common/home/handouts/list/index.tsx
+++ src/components/common/home/handouts/list/index.tsx
@@ -18,28 +18,28 @@ const HandOutsSection = ({ title, icon, link }: HandOutsBarProps) => {
           title='5월 5일 수업.pptx'
           period='2025.06.01'
           link=''
-          name='김은찬'
+          name='채근영'
         />
         <HandOutsItem
           status='hwp'
           title='파이썬 잘 하는 법.hwp'
           period='2025.05.20'
           link=''
-          name='채근영'
+          name='권대형'
         />
         <HandOutsItem
           status='pdf'
           title='파이썬 5월 수업 자료 정리.pdf'
           period='2025.05.19'
           link=''
-          name='김성한'
+          name='김은찬'
         />
         <HandOutsItem
           status='etc'
           title='파이썬 5월 수업 자료 정리.als'
           period='2025.05.19'
           link=''
-          name='한승환'
+          name='김성한'
         />
         <HandOutsItem
           status='etc'
@@ -50,10 +50,31 @@ const HandOutsSection = ({ title, icon, link }: HandOutsBarProps) => {
         />
         <HandOutsItem
           status='hwp'
-          title='컴일 석차 1등이 되는 법'
+          title='컴일 석차 1등이 되는 법.hwp'
           period='2025.05.19'
           link=''
-          name='권대형'
+          name='한승환'
+        />
+        <HandOutsItem
+          status='pptx'
+          title='정보 수업 첫 강의.pptx'
+          period='2025.05.18'
+          link=''
+          name='김은찬'
+        />
+        <HandOutsItem
+          status='pdf'
+          title='알고리즘 개념 총정리.pdf'
+          period='2025.05.17'
+          link=''
+          name='오용준'
+        />
+        <HandOutsItem
+          status='py'
+          title='list_comprehension.py'
+          period='2025.05.16'
+          link=''
+          name='채근영'
         />
       </S.HandOutsList>
     </S.Container>
