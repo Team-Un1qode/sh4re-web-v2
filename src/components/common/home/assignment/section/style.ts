49a1674f7d144a9cddfcd6e10480503a9c9c4e56
chaeyn <chaulfe@gmail.com>
2025-06-29 16:03:16 +0900

    feat: assignment 페이지 추가

--- src/components/common/home/assignment/section/style.ts
+++ src/components/common/home/assignment/section/style.ts
@@ -1,23 +1,34 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div<{ $variant: "home" | "detail" }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
   height: 100%;
-  background-color: ${({ theme }) => theme.color.background};
   box-shadow: 0px 3px 24px 0px ${({ theme }) => theme.color.borderGray};
   border-radius: 0.25rem;
   overflow: hidden;
-  &:first-child {
-    grid-row: 1 / span 2;
-  }
+
+  ${(props) =>
+    props.$variant === "home"
+      ? css`
+          background-color: ${({ theme }) => theme.color.background};
+
+          &:first-child {
+            grid-row: 1 / span 2;
+          }
+        `
+      : css`
+          min-height: 100vh;
+          background-color: ${({ theme }) => theme.color.backgroundWhite};
+        `}
 `;
 
-export const AssignmentList = styled.div`
+export const AssignmentList = styled.div<{
+  $variant?: "home" | "detail";
+}>`
   width: 100%;
-  height: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
--- src/pages/assignment/index.tsx
+++ src/pages/assignment/index.tsx
@@ -1,7 +1,9 @@
-// import * as S from "./style";
+import AssignmentSection from "../../components/common/home/assignment/section";
 
 const Assignment = () => {
-  return <div></div>;
+  return (
+    <AssignmentSection title='과제 목록' icon='' link='' variant='detail' />
+  );
 };
 
 export default Assignment;
